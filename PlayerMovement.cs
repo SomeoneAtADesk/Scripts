dbd519a33ef4c4d98ce144bc846516a91f7803f0
Sean-oreos <148512202+Child-Flesh@users.noreply.github.com>
2023-11-06 13:08:22 +1100

    Update PlayerMovement.cs
    Change from Y-Velocity check to a Raytracking Check.

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -4,22 +4,24 @@ public class PlayerMovment : MonoBehaviour
 {
 
   // Movement code from DistortedPixelStudios on yourube, video link - https://www.youtube.com/watch?v=n4N9VEA2GFo
-
+  
+  //State all Valuables
   public float MovementSpeed = 1.5f;
   public float JumpForce = 2;
   public Animator anim;
   public float delay = 0.4f;
+  public Vector2 Boxsize;
+  public float Castdistance;
+  public LayerMask PlatformLayer; //Set all objects that players want jumper to this Layer
 
   float timer;
   // _Attack;
   bool ADelay;
-
   private Rigidbody2D _rigidbody;
-
-  float horizontalMove = 0f;
 
     void Start() {
       _rigidbody = GetComponent<Rigidbody2D>();
+      anim = GetComponent<Animator>();
       ADelay = false;
       anim.SetBool("_Attack", false);
     }
@@ -29,14 +31,14 @@ public class PlayerMovment : MonoBehaviour
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
 
-        if (Input.GetButtonDown("Horizontal"))  {
+        if (Input.GetButton("Horizontal"))  {
         anim.SetBool("Running", true);
         }
         else if (Input.GetButtonUp("Horizontal")) {
           anim.SetBool("Running", false);
         }
 
-          Debug.Log(anim.GetBool("_Attack"));
+          //Debug.Log(anim.GetBool("_Attack"));
           // ^^ see when Attack on and off are triggered, helpful for fixing.
 
       // TODO: Prevent the glitch that occurs when spamming the click button rapidly
@@ -53,16 +55,42 @@ public class PlayerMovment : MonoBehaviour
             timer -= delay;
           }
         }
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f) {
-          _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
-          anim.SetBool("Jumping", true);
-        }
-        else if (Input.GetButtonUp("Jump")) {
-          anim.SetBool("Jumpung", false);
+        //Jump Variable Check
+        if (Input.GetButtonDown("Jump")) {
+            //Checks if the Variable isGround is true than applies Jump force and plays Jumping animation.
+            if (isGrounded() == true) {
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            anim.SetBool("Jumping", true);
+            }
+            //If player can't jump, sends debug message. Possiblity of SFX?
+            else {
+              Debug.Log("Player Can't Jump.");
+            }
         }
     }
+
+    //If clicking being held, turn off anim.
     void AttackOff() {
       anim.SetBool("_Attack", false);
-      anim.SetBool("ADelay", false);
     }
+    
+    //Jump Command Script from https://www.youtube.com/watch?v=P_6W-36QfLA
+    //Jump Command
+    public bool isGrounded() 
+    {
+      //Create A box based off the bunch of variables, not 100% what all do but 0 is angle so don't change.
+      if (Physics2D.BoxCast(transform.position, Boxsize, 0, -transform.up, Castdistance, PlatformLayer)) {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+    //Jump Box Visualizer
+    private void OnDrawGizmos() 
+    {
+      Gizmos.DrawWireCube(transform.position-transform.up * Castdistance, Boxsize);
+    } 
 }
